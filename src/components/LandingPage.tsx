a3cae4f86990f568ad9c9da24991f6ce4c121aca
Marcelo Kopmann <marcelokopmann@gmail.com>
2023-11-10 15:28:16 -0300

    refac: landing page sections

--- src/components/LandingPage.tsx
+++ src/components/LandingPage.tsx
@@ -1,6 +1,7 @@
 import { DISCORD_INVITE_URL } from '@/lib/constants'
 import Image from 'next/image'
 import Link from 'next/link'
+import { twMerge } from 'tailwind-merge'
 
 export function LandingPage() {
   return (
@@ -35,68 +36,65 @@ export function LandingPage() {
           </Link>
         </div>
       </section>
-      <section className="flex w-full flex-col lg:flex-row bg-[#a27a50] py-12 text-black">
-        <div className="p-8 lg:w-1/2">
-          <h2 className="mb-4 text-4xl font-bold">Eventos da Guilda</h2>
-          <p className="text-lg">
-            Nós organizamos regularmente uma variedade de eventos para nossos
-            membros. De incursões a festivais temáticos, sempre há algo
-            excitante acontecendo nos Corvos de Efrim.
-          </p>
-          {/* TODO: Criar página sobre eventos */}
-          {/* <Link
-            className="inline-flex h-10 items-center justify-center rounded-md border border-transparent bg-black px-5 text-base font-medium text-[#a27a50] hover:bg-black/90"
-            href="#"
-          >
-            Aprenda mais sobre nossos eventos
-          </Link> */}
-        </div>
-        <div className="px-8 lg:w-1/2">
-          <img
-            className="h-[250px] w-full lg:mx-auto lg:h-[250px] lg:w-[500px]"
-            alt="Guild Events Image"
-            height="500"
-            width="500"
-            src="/images/barco.png"
-            style={{
-              objectFit: 'cover',
-              borderRadius: '0.5rem',
-              border: '0.25rem solid black',
-            }}
-          />
-        </div>
-      </section>
-      <section className="flex w-full flex-col lg:flex-row-reverse bg-black py-12 text-white">
-        <div className="lg:w-1/2 p-8">
-          <h2 className="mb-4 text-4xl font-bold">Junte-se a Nós</h2>
-          <p className="mb-8 text-lg">
-            Estamos sempre procurando por novos membros para se juntar à nossa
-            tripulação. Descubra mais sobre nosso processo de recrutamento,
-            cargos disponíveis e outras regras.
-          </p>
-          <Link
-            className="inline-flex h-10 items-center justify-center rounded-md border border-transparent bg-[#a27a50] px-5 text-base font-medium text-white hover:bg-[#a27a50]/90"
-            href="/about"
-          >
-            Saiba mais sobre as nossas regras
-          </Link>
-        </div>
-        <div className="px-8 lg:w-1/2">
-          <img
-            className="h-[250px] w-full lg:mx-auto lg:h-[250px] lg:w-[500px]"
-            alt="Guild Events Image"
-            height="500"
-            width="500"
-            src="/images/juramento.png"
-            style={{
-              objectFit: 'cover',
-              borderRadius: '0.5rem',
-              border: '0.25rem solid #a27a50',
-            }}
-          />
-        </div>
-      </section>
-     
+      {
+        [
+          {
+            title: "Eventos da Guilda",
+            description: "Nós organizamos regularmente uma variedade de eventos para nossos membros. De incursões a festivais temáticos, sempre há algo excitante acontecendo nos Corvos de Efrim.", 
+            imageUrl: "/images/barco.png",
+          },
+          {
+            title: "Junte-se a Nós",
+            description: "Estamos sempre procurando por novos membros para se juntar à nossa tripulação. Descubra mais sobre nosso processo de recrutamento, cargos disponíveis e outras regras.",
+            link: "/about",
+            linkText: "Saiba mais sobre as nossas regras",
+            imageUrl: "/images/juramento.png",
+          },
+          // Add more sections as needed
+        ].map(
+          ({ title, description, imageUrl, link, linkText }, index) => {
+            const isOdd = index % 2 === 0
+            return (
+            <section
+              key={index}
+              className={twMerge('flex w-full flex-col lg:flex-row py-12',
+                isOdd ? "bg-[#a27a50] text-black" : "bg-black text-white lg:flex-row-reverse")}
+            >
+              <div className="p-8 lg:w-1/2">
+                <h2 className="mb-4 text-4xl font-bold">{title}</h2>
+                <p className="text-lg">{description}</p>
+                {link && (
+                  <Link
+                  className={twMerge(
+                    'inline-flex h-10 items-center justify-center rounded-md border border-transparent px-5 text-base font-medium mt-4',
+                    isOdd ? "bg-black text-[#a27a50] hover:bg-black/90" : "bg-[#a27a50] text-white hover:bg-[#a27a50]/90"
+                  )}
+                  
+                    href={link}
+                  >
+                    {linkText}
+                  </Link>
+                )}
+              </div>
+              <div className="px-8 lg:w-1/2">
+                <img
+                  className="h-[250px] w-full lg:mx-auto lg:h-[250px] lg:w-[500px]"
+                  alt={title}
+                  height={500}
+                  width={500}
+                  src={imageUrl}
+                  style={{
+                    objectFit: "cover",
+                    borderRadius: "0.5rem",
+                    border: `0.25rem solid ${isOdd ? "black" : "#a27a50"}`,
+                  }}
+                />
+              </div>
+            </section>)
+          }
+        )
+      }
+    
     </>
   )
 }
